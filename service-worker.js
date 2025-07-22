a50349da21f86b389ed84d42055d99b6f6fef060
JOAO FELIPE <86098025+Joaofelipe14@users.noreply.github.com>
2025-07-22 12:37:40 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(event) {
+});
+
+self.addEventListener('activate', function(event) {
+});
+
+self.addEventListener('notificationclick', function(event) {
+    if (event.action === 'focusWindow') {
+        clients.openWindow('/');
+    }
+    event.notification.close();
+});
+
+self.addEventListener('push', function(event) {
+    const options = {
+        body: event.data.text(),
+        icon: '/icon-notification-powerzap.png',
+        badge: '/icon-notification-powerzap.png',
+        vibrate: [200, 100, 200],
+        tag: 'powerzap'
+    };
+    
+    event.waitUntil(
+        self.registration.showNotification('Nova Notificação', options)
+    );
+});
